7d51dd59aac2e012043fe611a2e68fed90400230
Celso Ica <celsomti32@gmail.com>
2026-03-30 03:43:33 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBFD5KZ8sGfvXU-evgdRZMRMDOxsr0d1uo",
+  authDomain: "sons-bwe-fixes-20ea5.firebaseapp.com",
+  projectId: "sons-bwe-fixes-20ea5",
+  storageBucket: "sons-bwe-fixes-20ea5.firebasestorage.app",
+  messagingSenderId: "866364254394",
+  appId: "1:866364254394:web:c124524c368f1504ceba83",
+  measurementId: "G-KP0ZC6JTP9"
+};
